a16f77176b14a03698d570bfefa657202f55c816
Alison Sav <alisa.zarytska@troops-ext.fr>
2023-08-04 10:58:49 +0200

    Small fix for tox

--- core/celery.py
+++ core/celery.py
@@ -1,7 +1,8 @@
 import os
 
 from celery import Celery
-from celery.schedules import crontab
+
+# from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
